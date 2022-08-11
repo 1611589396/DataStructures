2c6bee3e1f096dd00ee9295483bb2afc17885f8e
拆骨为刀 <1611589396@qq.com>
2022-08-11 17:51:09 +0800

    init project

---
+++ sgg/src/test/java/com/atguigu/sort/SelectSortTest.java
@@ -0,0 +1,38 @@
+package com.atguigu.sort;
+
+import org.junit.Test;
+
+public class SelectSortTest {
+    @Test
+    public void selectSort() {
+        int arr[] = {101, 34, 119, 1};
+        SelectSort.selectSort(arr);
+    }
+    @Test
+    public void swap() {
+        int[] a = {5,6};
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        swap(a);
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        swap1(a);
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+    }
+    /**
+     * Java交换两个数字:https://www.cnblogs.com/zhihaospace/p/12217633.html
+     * java中两个变量，值的交换问题:https://blog.csdn.net/qq_42647047/article/details/117202134
+     * @param a
+     */
+    public static void swap(int[] a) {
+        int tmp = a[0];
+        a[0] = a[1];
+        a[1] = tmp;
+    }
+    public static void swap1(int[] a) {
+        int tmp = a[1];
+        a[1] = a[0];
+        a[0] = tmp;
+    }
+}
